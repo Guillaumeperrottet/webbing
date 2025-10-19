6c382563ca4bb66b6ba89b4a65c09a058557eb16
Guillaumeperrottet <perrottet.guillaume.97@gmail.com>
2025-10-19 11:30:11 +0200

    titre page accueil plus haut

--- src/app/applications/page.tsx
+++ src/app/applications/page.tsx
@@ -131,6 +131,7 @@ export default function ApplicationsPage() {
             </>
           }
           showSeparator={true}
+          className="py-8 sm:py-12 md:py-16 lg:py-20 xl:py-24"
         />
       </div>
 
